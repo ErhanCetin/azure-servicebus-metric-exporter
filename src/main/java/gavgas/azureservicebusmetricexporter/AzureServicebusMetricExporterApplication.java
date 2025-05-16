896d1d893c862b7e2068f9f785d5941b7d8fcbaf
Erhan Cetin <e.cetin@netconomy.net>
2025-05-16 15:30:20 +0200

    NOTASK initial commit

---
+++ src/main/java/gavgas/azureservicebusmetricexporter/AzureServicebusMetricExporterApplication.java
@@ -0,0 +1,13 @@
+package gavgas.azureservicebusmetricexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureServicebusMetricExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureServicebusMetricExporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gavgas/azureservicebusmetricexporter/AzureServicebusMetricExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package gavgas.azureservicebusmetricexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureServicebusMetricExporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
